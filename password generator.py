847c03f0fbe83de9dbe028af957b324c06372cc9
RCKNDRLLA <sokolov.yakov@inbox.ru>
2024-01-10 00:43:38 +0500

    Added lenth switchers

--- password generator.py
+++ password generator.py
@@ -3,16 +3,32 @@ from tkinter import *
 import pyperclip
 
 symbols = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&*+-=?@^_'
+lenth = 8
 
-print('Укажите количество паролей для генерации')
-quantity = int(input())
-print('Укажите длину одного пароля')
-lenth = int(input())
+# create a window
+tk = Tk()
+tk.resizable(False, False)
+tk.title("Password generator")
+tk.geometry("500x200")
+text_var = StringVar()
+
+# create a label
+password_label = Label(tk, textvariable=text_var, width=20, font='corbel 13')
+password_label.grid(row=0, column=1, columnspan=2, pady=(25, 20))
+
+# Создаем Canvas для значка копирования
+canvas = Canvas(tk, width=22, height=22)
+canvas.grid(row=0, column=2, pady=(25, 10), padx=(10, 10))
+
+# Создаем значок копирования (два прямоугольника)
+canvas.create_rectangle(5, 5, 15, 15, outline='black')  # Задний прямоугольник
+canvas.create_rectangle(10, 10, 20, 20, outline='black', fill=tk.cget("bg"))  # Передний прямоугольник
 
 
+# создаем функцию выделения значка копирования
 def on_enter(event):
-    canvas.config(bg='blue')
-    canvas.create_rectangle(10, 10, 20, 20, outline='black', fill='blue')
+    canvas.config(bg='lightblue')
+    canvas.create_rectangle(10, 10, 20, 20, outline='black', fill='lightblue')
 
 
 def on_leave(event):
@@ -26,29 +42,12 @@ def generate_password():
     return ''.join(password)
 
 
+# создаем функцию очистки значка копирования
 def clear_checkmark():
     canvas.delete("all")
     canvas.create_rectangle(5, 5, 15, 15, outline='black')  # Задний прямоугольник
     canvas.create_rectangle(10, 10, 20, 20, outline='black', fill=tk.cget("bg"))  # Передний прямоугольник
 
-
-# create a window
-tk = Tk()
-tk.title("Password generator")
-tk.geometry("500x120")
-text_var = StringVar()
-
-# create a label
-password_label = Label(tk, textvariable=text_var, font='corbel 13')
-password_label.grid(row=0, column=1, pady=(25, 0), padx=(150, 10))
-
-# Создаем Canvas для кнопки копирования
-canvas = Canvas(tk, width=22, height=22)
-canvas.grid(row=0, column=2, pady=(25, 0), padx=(10, 10))
-
-# Создаем значок копирования (два прямоугольника)
-canvas.create_rectangle(4, 5, 15, 15, outline='black')  # Задний прямоугольник
-canvas.create_rectangle(10, 10, 20, 20, outline='black', fill=tk.cget("bg"))  # Передний прямоугольник
 
 canvas.bind("<Enter>", on_enter)
 canvas.bind("<Leave>", on_leave)
@@ -78,14 +77,44 @@ def change_password():
     new_password = generate_password()
     text_var.set(new_password)
     clear_checkmark()
-    print(new_password)
 
     # Привязываем событие клика к функции копирования
     canvas.bind("<Button-1>", lambda event: copy_to_clipboard(new_password))
 
 
-button = Button(tk, text="Generate", width=20, font='corbel 13', pady=0, command=change_password)
-button.grid(row=1, column=1, columnspan=2, pady=10, padx=150)
+# создаем функции уменьшения и увеличения длинны пароля при нажатии кнопок
+def reducing_length():
+    global lenth
+    lenth -= 1
+    text_var.set(generate_password())
+    change_quantity()
+
+
+def increasing_length():
+    global lenth
+    lenth += 1
+    text_var.set(generate_password())
+    change_quantity()
+
+
+button = Button(tk, text="generate", width=20, font='corbel 13', pady=0, command=change_password)
+button.grid(row=1, column=1, columnspan=2, pady=0, padx=150)
+
+button1 = Button(tk, text='-', width=5, font='Corbel 13', pady=0, command=reducing_length)
+button1.grid(row=1, column=1, columnspan=1, pady=0, padx=90, sticky='w')
+
+button2 = Button(tk, text='+', width=5, font='Corbel 13', pady=0, command=increasing_length)
+button2.grid(row=1, column=2, columnspan=1, pady=0, padx=90, sticky='e')
+
+
+quantity = IntVar(value=8)
+quantity_label = Label(tk, text="количество символов =", textvariable=quantity, font='corbel 15')
+quantity_label.grid(row=2, column=1, columnspan=2, pady=0, padx=0)
+
+
+# функция изменения длинны пароля
+def change_quantity():
+    quantity.set(lenth)
 
 
 def checkbox_status():
@@ -105,9 +134,9 @@ var1 = IntVar(value=1)
 var2 = IntVar(value=1)
 
 # Создаем Checkbuttons
-checkbox1 = Checkbutton(tk, text="Использовать цифры", variable=var1, command=checkbox_status)
-checkbox1.grid(row=3, column=1, columnspan=2, pady=10, padx=150)
-checkbox2 = Checkbutton(tk, text='Использовать символы', variable=var2, command=checkbox_status)
-checkbox2.grid(row=4, column=1, columnspan=2, pady=10, padx=150)
+checkbox1 = Checkbutton(tk, text='use digits', variable=var1, command=checkbox_status)
+checkbox1.grid(row=3, column=1, columnspan=2, pady=0, padx=150)
+checkbox2 = Checkbutton(tk, text='use symbols', variable=var2, command=checkbox_status)
+checkbox2.grid(row=4, column=1, columnspan=2, pady=0, padx=150)
 
 tk.mainloop()
